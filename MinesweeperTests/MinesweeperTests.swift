d66e2e6e3c1b77e67f57f50c28beedaf616022ed
Laurala1006 <l20130716@gmail.com>
2025-04-04 22:21:46 +0800

    Initial Commit

---
+++ MinesweeperTests/MinesweeperTests.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperTests.swift
+//  MinesweeperTests
+//
+//  Created by 吳育臻 on 2025/4/4.
+//
+
+import Testing
+@testable import Minesweeper
+
+struct MinesweeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
